97af2e8e708bf86f11b5dd7a6b25054cf9d72833
JoanLopezGit <123471579+JoanLopezGit@users.noreply.github.com>
2023-03-03 09:53:42 +0100

    Update DDL_Eleccions2019_v2.sql

--- DDL_Eleccions2019_v2.sql
+++ DDL_Eleccions2019_v2.sql
@@ -18,9 +18,6 @@ ADD	CONSTRAINT fk_eleccions_municipis_municipis FOREIGN KEY (municipi_id)
 ALTER TABLE candidatures
 ADD CONSTRAINT fk_candidatures_eleccions FOREIGN KEY (eleccio_id)
 		REFERENCES eleccions(eleccio_id);
-
-ALTER TABLE persones
-MODIFY COLUMN dni CHAR(10);
         
 ALTER TABLE candidats
 ADD CONSTRAINT fk_candidats_candidatures FOREIGN KEY (candidatura_id)
